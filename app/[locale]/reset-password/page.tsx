e5b63bd56920270b35712d9365c21b8fd6b53fd9
ProdanCristian <prodancristianbusiness@gmail.com>
2024-11-17 15:03:19 +0000

    ss

--- app/[locale]/reset-password/page.tsx
+++ app/[locale]/reset-password/page.tsx
@@ -3,8 +3,12 @@ import { ResetPasswordForm } from "@/components/auth/ResetPasswordForm";
 export default function ResetPasswordPage({
   searchParams,
 }: {
-  searchParams: { token: string };
-}) {
+  searchParams: { token?: string }
+} & { searchParams: any }) {
+  if (!searchParams.token) {
+    return <div>Invalid or missing reset token</div>;
+  }
+
   return (
     <div className="my-32 flex items-center justify-center">
       <ResetPasswordForm token={searchParams.token} />
